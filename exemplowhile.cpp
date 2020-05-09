22be95b13b4bcf904bc9b590ea6d6e6a533772ee
Antoniocfilho <63979255+Antoniocfilho@users.noreply.github.com>
2020-05-09 18:58:20 -0300

    Create exemplowhile.cpp

---
+++ exemplowhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int controle = 56563456345654654674574574575457457;
+	
+	while (controle < 5)
+	{
+		cout<<"\n Bom Dia ";
+		controle = controle * controle;
+		cout<<controle <<"\n";
+	}
+	return 0;
+	system ("PAUSE");
+}
