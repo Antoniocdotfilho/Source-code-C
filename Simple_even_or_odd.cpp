d5af66ab62f56a94c619ed6604f86c3a657e7903
Antoniocfilho <63979255+Antoniocfilho@users.noreply.github.com>
2020-05-09 18:53:08 -0300

    Create Simple_even_or_odd.cpp

---
+++ Simple_even_or_odd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int x, y, maior;
+cout << "Digite o numero 1: ";
+cin >> x;
+cout << "Digite o numero 2: ";
+cin >> y;
+
+if(x%2==0)
+{
+	cout << "É par";
+}
+else{
+	cout << "É impar";
+}
+return 0;
+system("PAUSE");
+} // Fim de main()
