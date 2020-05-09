6d757dc3c62c28c85b1d8c5e8bdf8792adc84ec2
Antoniocfilho <63979255+Antoniocfilho@users.noreply.github.com>
2020-05-09 18:56:27 -0300

    Create Triangle.cpp

---
+++ Triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	float base, altura, calculo;
+	cout << "Digite a altura do retangulo";
+	cin >> altura;
+	cout << "Digite abase do retangulo";
+	cin >> base;
+	calculo = (base*altura);
+	cout << "A área do retangulo é "<<calculo;
+	return 0;
+	system("PAUSE");
+}
